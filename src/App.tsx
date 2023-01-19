54206be8f2e5bd7514147c36344f0f53ef4cdcac
semanurakkuzu <semanurakkuzu@gmail.com>
2023-01-19 14:43:09 +0300

    changed app component to use location component

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import Locations from './components/Locations'
+import './style/style.scss'
+function App() {
+  return (
+    <div className='App'>
+      <Locations></Locations>
+    </div>
+  )
+}
+
+export default App
